2b98bbf75ec72c3c0493dce3740709349cde4d56
Michael Diaz <michaeldiaz.compsci@gmail.com>
2020-05-28 02:13:13 -0400

    Create vm.c
    Began psudeocode structure

---
+++ vm.c
@@ -0,0 +1,25 @@
+// Michael Diaz
+// Gregory Allen
+// COP 3402, Summer 2020
+
+void main(int *argc, char **argv)
+{
+  //make a data struct to store the instruction and break it into op, l and m
+
+  //struct *input
+  //int *code, *stack
+  //int programCounter = 0, basePointer = 1, stackPointer = 0
+  //bool pcOverride = false
+
+  //get the name of the input file
+  //loop through each line; making it a struct, add struct to the dynamic array
+  
+  //while input[programCounter].op != return
+    /*
+    pcOverride = false
+    //switch statement for the input[programCounter].op
+      /*
+      //go through all the possible operations, do whatever is needed
+      */
+    */
+}

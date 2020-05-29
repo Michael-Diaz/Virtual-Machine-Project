ca1826637a6d033d99ba7fe1c7de5d65915dd381
Michael Diaz <michaeldiaz.compsci@gmail.com>
2020-05-29 09:46:16 -0400

    Update vm.c

--- vm.c
+++ vm.c
@@ -13,19 +13,17 @@ typedef struct
 
 void main(int argc, char **argv)
 {
-  instruction *input;
-  input = (int)malloc(0);
+  instruction *input = (int)malloc(sizeof(instruction) * 0);
   int inputSize = 0;
   
   //int *code, *stack;
-  //int programCounter = 0, basePointer = 1, stackPointer = 0
-  //bool pcOverride = false
+  int programCounter = 0, basePointer = 1, stackPointer = 0
 
   //get the name of the input file
   FILE *ifp = fopen(argv[1], "r");
   char buffer[1023];
   char *word;
-  int i = 0;
+  int i = 0, j, k;
   
   //loop through each line; making it a struct, add struct to the dynamic array
   while (fscanf(ifp, "%s", buffer) != EOF)
@@ -33,24 +31,38 @@ void main(int argc, char **argv)
     word = malloc(sizeof(char) * (strlen(buffer) + 1));
     strcpy(word, buffer);
     
+    // With every line, add an instruction to the array
     if (i % 3 == 0)
     {
+      printf("\n"); // REMOVE LINE WHEN COMPLETE
       inputSize++;
-      input = (int)realloc(input, inputSize);
+      input = (int)realloc(input, sizeof(instruction) * inputSize);
     }
     
-    printf("%s", word);
-    input[inputSize - 1].line[i % 3] = atoi(word)
+    printf("%s", word); // REMOVE LINE WHEN COMPLETE
+    input[inputSize - 1].line[i % 3] = atoi(word);
     i++;
 
     free(word);
   }
   
+  // Print the input file
+  printf("\n");
+  for (j = 0; j < i - 1; j++)
+  {
+    for (k = 0; k < 3; k++)
+    {
+      printf("%d", input[j].line[k]);
+    }
+    printf("\n"); 
+  }
+  
   //while input[programCounter].op != return
-    /*
-    pcOverride = false
+    
     //switch statement for the input[programCounter].op
       /*
       //go through all the possible operations, do whatever is needed
       */
+  
+  free(input);
 }

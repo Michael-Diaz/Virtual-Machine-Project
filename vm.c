6fc40dfbd7b40ac44ea0d123b28783ddc6f2bb7d
Michael Diaz <michaeldiaz.compsci@gmail.com>
2020-05-29 13:42:24 -0400

    Update vm.c
    Slight modifications in printf statements, addition of a string field for the "op" name, better commentation

--- vm.c
+++ vm.c
@@ -6,59 +6,64 @@
 #include <stdlib.h>
 #include <string.h>
 
+// This struct contains the opcode, lexicographical level, and parameter field
 typedef struct Instruction
 {
-  int line[3]; // OP, then L, then M
+  int components[3]; // OP, then L, then M
+  char *opName;
 } Instruction;
 
 void main(int argc, char **argv)
 {
+  // Create an array to manipulate/access the instructions for the PM/0 and set its size to 0
   Instruction *input = malloc(sizeof(Instruction));
   int inputSize = 0;
 
-  //int *code, *stack;
+  //~~int *code, *stack;~~
   int programCounter = 0, basePointer = 1, stackPointer = 0;
 
-  //get the name of the input file
+  // Set up the input file variable, along with buffer and allocation for the current word being scanned
   FILE *ifp;
   char buffer[1023];
   char *word;
   int i = 0, j, k;
 
+  // If no filename is given, then the program exits
   if (argc > 1)
      ifp = fopen(argv[1], "r");
   else
     exit(0);
 
-  //loop through each line; making it a struct, add struct to the dynamic array
+  // Read the file, adding the components to an instruction set and adding it to the array in the order recieved
   while (fscanf(ifp, "%s", buffer) != EOF)
   {
     word = malloc(sizeof(char) * (strlen(buffer) + 1));
     strcpy(word, buffer);
 
-    // With every line, add an instruction to the array
+    // With every line, add an instruction slot to the array
     if (i % 3 == 0)
     {
-      printf("\n"); // REMOVE LINE WHEN COMPLETE
+      
       inputSize++;
       input = realloc(input, sizeof(Instruction) * inputSize);
     }
 
-    printf("%s", word); // REMOVE LINE WHEN COMPLETE
-    input[inputSize - 1].line[i % 3] = atoi(word);
+    // Fill the instruction's "component" arrays with the appropriate data
+    input[inputSize - 1].components[i % 3] = atoi(word);
     i++;
 
+    // Free the current scanned word to avoid a mem-leak
     free(word);
   }
 
   // Print the input file
-  printf("\n");
+  printf("Line  OP    L     M\n");
   for (j = 0; j < inputSize; j++)
   {
-    printf("%d", j);
+    printf("%-6d", j);
     for (k = 0; k < 3; k++)
     {
-      printf(" %d", input[j].line[k]);
+      printf("%-6d", input[j].components[k]);
     }
     printf("\n");
   }
